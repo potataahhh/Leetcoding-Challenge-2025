d1a58c0029fbdd6f39c3120a6d213dcd59c9d2a3
Pew-Pew <155428667+potataahhh@users.noreply.github.com>
2025-10-08 21:51:46 +0530

    Update 2300. Successful Pairs of Spells and Potions.cpp

--- Oct Leetcode Challenge/Day 8/2300. Successful Pairs of Spells and Potions.cpp
+++ Oct Leetcode Challenge/Day 8/2300. Successful Pairs of Spells and Potions.cpp
@@ -4,10 +4,38 @@ public:
        sort(potions.begin(),potions.end());
        vector<int>res;
        for(int temp:spells){
+
+           // getting the threshold
+           
         long point = ceil((double)success/temp);
+
+           // Binary search using lower_bound
+           
         auto it = lower_bound(potions.begin(),potions.end(),point);
         res.push_back(potions.end()-it);
        }
        return res;
     }
 };
+
+
+
+========================================================================================
+
+// Instead of ceil we can also use 
+
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(),potions.end());
+        vector<int>res;
+        for(int temp:spells){
+            long point=(success+temp-1)/temp;
+            auto it = lower_bound(potions.begin(),potions.end(),point);
+            res.push_back(potions.end()-it);    
+        }
+        return res;
+    }
+};
+
+
